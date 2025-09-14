0e2872a7c012d3eaa92a7b7f4e96567416ec6fe1
brysonbw <brysonward77@gmail.com>
2025-09-13 19:44:34 -0600

    Add JSDoc comments for functions

--- src/utils/dateFunctions.ts
+++ src/utils/dateFunctions.ts
@@ -1,3 +1,4 @@
+/** Get current date year */
 export function currentYear(): string {
   const date = new Date();
   return date.getFullYear().toString();
--- src/utils/helperFunctions.ts
+++ src/utils/helperFunctions.ts
@@ -1,4 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
+
+/** Get UUIDv4 */
 export function UUIDv4(): string {
   if (crypto && typeof crypto?.randomUUID === 'function') {
     return crypto.randomUUID();
